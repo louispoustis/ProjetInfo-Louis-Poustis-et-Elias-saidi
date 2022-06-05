3c9ab25f53d7ccbe2618b36c76e2c49b46d7c37b
Alexandra <103836628+abaivel@users.noreply.github.com>
2022-06-05 23:58:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "fonctions.h"
 
 int main()
 {
-    int grilletetris[10][10]={{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}};
+    srand(time(NULL));
+    int grille[10][10]={{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}};
     Piece tab_pieces[7][4];
     creationPiece(Piece tabPiece){
- int orientation, colonne;
- int fin=0
-     while(fin==0){
-     printf("Entrez l'orientation:/n"); 
-     scanf("%d",&orient);
-     printf("Entrez la colonne:/n");
-     scanf("%c",&colonne);
-        
- placement(grille[TAILLE][TAILLE],Piece piece_hasard,int col){        
-         
- eliminer_ligne(grille[TAILLE][TAILLE]){   
-     
-     return 0;
+    int orient, colonne;
+    int fin=0
+    while(fin==0){
+        affiche(grille);
+        Piece piece_hasard[4]=tab_pieces[rand()%7];
+        printf("Entrez l'orientation:/n"); 
+        scanf("%d",&orient);
+        printf("Entrez la colonne:/n");
+        scanf("%c",&colonne);
+        fin=placement(grille,piece_hasard[orient],colonne){        
+        eliminer_ligne(grille){   
+    }
+    printf("Fin du jeu\n");
+    return 0;
 }
