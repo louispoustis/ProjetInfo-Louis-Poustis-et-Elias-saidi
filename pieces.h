3e72e7fb01dfc9abfe402fb6e70955b674e44483
Alexandra <103836628+abaivel@users.noreply.github.com>
2022-06-05 22:59:56 +0200

    Update pieces.h

--- pieces.h
+++ pieces.h
@@ -1,1 +1,21 @@
+#define TAILLE 10
 
+typedef struct {
+	int ligne;
+	int colonne;
+	int forme[4][4];
+} Piece;
+
+void cpy_tab2D(int copie_tab[4][4],int tab_a_copie[4][4]);
+
+void creationPiece(Piece tabPiece[7]);
+
+int randompiece();
+
+Piece randompiece(Piece tabPiece[7]);
+
+int placement (int grille[TAILLE][TAILLE],Piece tabpiece[7],int col);
+
+void echange_ligne(int grille[TAILLE][TAILLE],int ligne1, int ligne 2);
+
+void eliminer_ligne(int grille[TAILLE][TAILLE]);
