d28e093d93ce7664966f45eadffcf4e385890451
Alexandra <103836628+abaivel@users.noreply.github.com>
2022-06-05 23:25:57 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -6,18 +6,14 @@ typedef struct {
 	int forme[4][4];
 } Piece;
 
-void affiche(grilletetris **tab);
+void affiche(int tab[TAILLE][TAILLE]);
 
 void cpy_tab2D(int copie_tab[4][4],int tab_a_copie[4][4]);
 
-void creationPiece(Piece tabPiece[7]);
+void creationPiece(Piece tabPiece[7][4]);
 
-int randompiece();
+int placement (int grille[TAILLE][TAILLE],Piece piece_hasard,int col);
 
-Piece randompiece(Piece tabPiece[7]);
-
-int placement (int grille[TAILLE][TAILLE],Piece tabpiece[7],int col);
-
-void echange_ligne(int grille[TAILLE][TAILLE],int ligne1, int ligne 2);
+void echange_ligne(int grille[TAILLE][TAILLE],int ligne1, int ligne2);
 
 void eliminer_ligne(int grille[TAILLE][TAILLE]);
